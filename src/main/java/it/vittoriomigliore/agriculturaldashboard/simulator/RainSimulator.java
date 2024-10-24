63ea9a547e0c0054ae61099fb0d53f046cd179f1
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-10-24 02:02:21 +0200

    simulator outline

---
+++ src/main/java/it/vittoriomigliore/agriculturaldashboard/simulator/RainSimulator.java
@@ -0,0 +1,8 @@
+package it.vittoriomigliore.agriculturaldashboard.simulator;
+
+public class RainSimulator {
+
+    public int generate() {
+        return 0;
+    }
+}
---
+++ src/main/java/it/vittoriomigliore/agriculturaldashboard/simulator/TemperatureSimulator.java
@@ -0,0 +1,8 @@
+package it.vittoriomigliore.agriculturaldashboard.simulator;
+
+public class TemperatureSimulator {
+
+    public double generate() {
+        return 0;
+    }
+}
