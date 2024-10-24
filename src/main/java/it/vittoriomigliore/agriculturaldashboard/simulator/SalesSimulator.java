138929d4f6ce11355e45deaa90fcbdbbc113912a
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-10-25 00:27:37 +0200

    poisson placed on simulator utils

--- src/main/java/it/vittoriomigliore/agriculturaldashboard/simulator/SalesSimulator.java
+++ src/main/java/it/vittoriomigliore/agriculturaldashboard/simulator/SalesSimulator.java
@@ -3,18 +3,16 @@ package it.vittoriomigliore.agriculturaldashboard.simulator;
 import java.util.Random;
 
 public class SalesSimulator {
-    private final Random random;
     private double dailySalesRate; // Average number of sales per day (λ)
 
     // Constructor
     public SalesSimulator(double initialDailySalesRate) {
-        this.random = new Random();
         this.dailySalesRate = initialDailySalesRate;
     }
 
     // Simulate daily sales using a Poisson distribution
     public int simulateDailySales() {
-        return generatePoissonSample(dailySalesRate);
+        return SimulatorUtils.poisson(dailySalesRate);
     }
 
     // Simulate sales for a month (30 days)
@@ -29,18 +27,6 @@ public class SalesSimulator {
     // Update daily sales rate to simulate seasonal changes or trends
     public void updateDailySalesRate(double newRate) {
         this.dailySalesRate = newRate;
-    }
-
-    // Generate a Poisson-distributed value
-    private int generatePoissonSample(double lambda) {
-        double l = Math.exp(-lambda);
-        int k = 0;
-        double p = 1.0;
-        do {
-            k++;
-            p *= random.nextDouble();
-        } while (p > l);
-        return k - 1;
     }
 
     // Getters for testing
