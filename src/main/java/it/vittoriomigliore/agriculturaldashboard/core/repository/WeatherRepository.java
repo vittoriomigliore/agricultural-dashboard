e81abca6a7784647825c42230515f094fe47578a
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-10-26 08:29:17 +0200

    fix weather repository

--- src/main/java/it/vittoriomigliore/agriculturaldashboard/core/repository/WeatherRepository.java
+++ src/main/java/it/vittoriomigliore/agriculturaldashboard/core/repository/WeatherRepository.java
@@ -1,7 +1,7 @@
 package it.vittoriomigliore.agriculturaldashboard.core.repository;
 
-import it.vittoriomigliore.agriculturaldashboard.core.entity.Sale;
+import it.vittoriomigliore.agriculturaldashboard.core.entity.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WeatherRepository extends JpaRepository<Sale, Integer> {
+public interface WeatherRepository extends JpaRepository<Weather, Integer> {
 }
