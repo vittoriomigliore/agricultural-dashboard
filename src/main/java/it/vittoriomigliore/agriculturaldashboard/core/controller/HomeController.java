1ce352b8213a1624ad11718176806b8f53d15b77
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-11-03 09:41:40 +0100

    field sections retrieved from database

--- src/main/java/it/vittoriomigliore/agriculturaldashboard/core/controller/HomeController.java
+++ src/main/java/it/vittoriomigliore/agriculturaldashboard/core/controller/HomeController.java
@@ -1,12 +1,31 @@
 package it.vittoriomigliore.agriculturaldashboard.core.controller;
 
+import it.vittoriomigliore.agriculturaldashboard.core.entity.Field;
+import it.vittoriomigliore.agriculturaldashboard.core.service.FieldService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    private final FieldService fieldService;
+
+    public HomeController(FieldService fieldService) {
+        this.fieldService = fieldService;
+    }
+
     @GetMapping("/")
     public String index() {
-        return "index"; // This refers to the index.html in src/main/resources/templates
+        return "index";
+    }
+
+    @ModelAttribute
+    public void addFields(Model model) {
+        List<Field> fields = fieldService.getAllFields();
+        model.addAttribute("fields", fields);
     }
 }
