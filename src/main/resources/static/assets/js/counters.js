f3dae873a207dc8362f88247145f7479c792a16e
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-11-07 23:34:31 +0100

    counters format number

--- src/main/resources/static/assets/js/counters.js
+++ src/main/resources/static/assets/js/counters.js
@@ -1,13 +1,13 @@
 const counters = [{
     element: document.getElementById('counter-today-income'),
     type: 'todaySales'
-},{
+}, {
     element: document.getElementById('counter-income'),
     type: 'sales'
-},{
+}, {
     element: document.getElementById('counter-costs'),
     type: 'cost'
-}  ]
+}]
 
 let wsUrlCounters = getWebSocketUrl("counters-live-data");
 
@@ -18,7 +18,7 @@ socketCounters.onmessage = function (event) {
     counters.forEach((counter) => {
         let resCounter = resCounters.find((c) => c.type === counter.type);
         if (resCounter !== undefined) {
-            counter.element.getElementsByClassName('counter-value')[0].textContent = resCounter.value;
+            counter.element.getElementsByClassName('counter-value')[0].textContent = formatNumber(resCounter.value);
             counter.element.getElementsByClassName('counter-change')[0].textContent = resCounter.change;
             counter.element.getElementsByClassName('progress-bar')[0].style.width = resCounter.change + "%";
             counter.element.getElementsByClassName('progress-bar')[0].setAttribute('aria-valuenow', resCounter.change);
--- src/main/resources/static/assets/js/utils.js
+++ src/main/resources/static/assets/js/utils.js
@@ -21,3 +21,11 @@ function getWebSocketUrl(path) {
     const host = window.location.host;
     return `${protocol}://${host}/${path}`;
 }
+
+function formatNumber(num) {
+    if (num >= 1e12) return (num / 1e12).toFixed(1).replace(/\.0$/, '') + 'T'; // Trillions
+    if (num >= 1e9) return (num / 1e9).toFixed(1).replace(/\.0$/, '') + 'B';  // Billions
+    if (num >= 1e6) return (num / 1e6).toFixed(1).replace(/\.0$/, '') + 'M';  // Millions
+    if (num >= 1e3) return (num / 1e3).toFixed(1).replace(/\.0$/, '') + 'K';  // Thousands
+    return num.toString(); // Less than 1,000
+}
