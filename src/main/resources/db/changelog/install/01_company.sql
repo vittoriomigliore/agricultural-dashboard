4df0c3e9a68d86dd94df44369dad3e52f2db88a5
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-11-25 23:51:41 +0100

    converted h2 db scripts to platform independent db scripts

--- src/main/resources/db/changelog/install/01_company.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
---changeset vmigliore:01_company runOnChange:true
-CREATE TABLE IF NOT EXISTS company
-(
-    company_id       INT PRIMARY KEY,
-    name             VARCHAR(255)
-);
--- src/main/resources/db/changelog/install/02_crop.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
---changeset vmigliore:02_crop runOnChange:true
-CREATE TABLE IF NOT EXISTS crop
-(
-    crop_id          INT PRIMARY KEY,
-    name             VARCHAR(255)
-);
